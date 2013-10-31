cac672f2398d64269288276c09e5012fc5d51f0f
Mahendri Winata <mahen.0112@gmail.com>
2013-10-31 15:40:35 +0700

    add favicon comradecms

--- comradecms/views/admin/layout/login.php
+++ comradecms/views/admin/layout/login.php
@@ -2,11 +2,15 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title><?php echo $title; ?></title>
+    <title>ComradeCMS <?php if (isset($title)) echo '- ' . $title; ?></title>
     <?php $this->load->view('admin/element/general/meta'); ?>
     <?php $this->load->view('admin/element/general/css'); ?>
     <?php $this->load->view('admin/element/general/js'); ?>
     <link rel="shortcut icon" href="<?php echo admin_base_url('ico'); ?>favicon.ico"/>
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo admin_base_url('ico'); ?>apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo admin_base_url('ico'); ?>apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo admin_base_url('ico'); ?>apple-touch-icon-57-precomposed.png">
   </head>
   <body>
     <div class="navbar navbar-fixed-top">
