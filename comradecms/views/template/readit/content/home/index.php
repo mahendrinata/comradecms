c3b9b8c0adcb84d592d37933442afb18786a0b11
Mahendri Winata <mahen.0112@gmail.com>
2013-12-05 07:29:48 +0700

    index home

--- comradecms/views/template/readit/content/home/index.php
+++ comradecms/views/template/readit/content/home/index.php
@@ -1,85 +1,23 @@
 <div id="home" class="group">
   <div id="timeline" class="col span_2_of_3">  
-    <div class="timeline-item">
-      <div class="timeline-author-date"><span>16/02/2013</span></div>
-      <a href="blog.html" class="timeline-author-link">
-        <img alt="avatar" src="images/avatars/avatar.png">
-      </a>
-      <a href="blog.html" class="timeline-item-link">
-        <div class="timeline-item-content">
-          <div class="timeline-item-cover">
-            <img src="images/cover/paris.jpg" alt="paris">
+    <?php foreach ($contents as $content) { ?>
+      <div class="timeline-item">
+        <div class="timeline-author-date"><span><?php ?></span></div>
+        <a href="blog.html" class="timeline-author-link">
+          <img alt="avatar" src="images/avatars/avatar.png">
+        </a>
+        <a href="blog.html" class="timeline-item-link">
+          <div class="timeline-item-content">
+            <div class="timeline-item-cover">
+              <img src="images/cover/paris.jpg" alt="paris">
+            </div>
+            <h2>Plongée dans le fantastique social</h2>
+            <p>Connaissez-vous l’homme-aquarium ? C’est un camelot du marché d’Aligre qui avale couramment cinq litres d’eau, puis cinq petites grenouilles, devant les yeux des badauds ébahis, avant de les recracher, flotte et rainettes bien vivantes, et de faire sa quête…</p>
+            <div class="timeline-item-meta"><span class="timeline-item-author">Félix Bayart</span>, <span class="timeline-item-comments-count">18 comments</span></div>
           </div>
-          <h2>Plongée dans le fantastique social</h2>
-          <p>Connaissez-vous l’homme-aquarium ? C’est un camelot du marché d’Aligre qui avale couramment cinq litres d’eau, puis cinq petites grenouilles, devant les yeux des badauds ébahis, avant de les recracher, flotte et rainettes bien vivantes, et de faire sa quête…</p>
-          <div class="timeline-item-meta"><span class="timeline-item-author">Félix Bayart</span>, <span class="timeline-item-comments-count">18 comments</span></div>
-        </div>
-      </a>
-    </div>
-    <div class="timeline-item">
-      <div class="timeline-author-date"><span>16/02/2013</span></div>
-      <a href="blog.html" class="timeline-author-link">
-        <img alt="avatar" src="images/avatars/avatar.png">
-      </a>
-      <a href="blog.html" class="timeline-item-link">
-        <div class="timeline-item-content">
-          <div class="timeline-item-cover">
-            <img src="images/cover/paris.jpg" alt="paris">
-          </div>
-          <h2>Plongée dans le fantastique social</h2>
-          <p>Connaissez-vous l’homme-aquarium ? C’est un camelot du marché d’Aligre qui avale couramment cinq litres d’eau, puis cinq petites grenouilles, devant les yeux des badauds ébahis, avant de les recracher, flotte et rainettes bien vivantes, et de faire sa quête…</p>
-          <div class="timeline-item-meta"><span class="timeline-item-author">Félix Bayart</span>, <span class="timeline-item-comments-count">18 comments</span></div>
-        </div>
-      </a>
-    </div>
-    <div class="timeline-item">
-      <div class="timeline-author-date"><span>16/02/2013</span></div>
-      <a href="blog.html" class="timeline-author-link">
-        <img alt="avatar" src="images/avatars/avatar.png">
-      </a>
-      <a href="blog.html" class="timeline-item-link">
-        <div class="timeline-item-content">
-          <div class="timeline-item-cover">
-            <img src="images/cover/paris.jpg" alt="paris">
-          </div>
-          <h2>Plongée dans le fantastique social</h2>
-          <p>Connaissez-vous l’homme-aquarium ? C’est un camelot du marché d’Aligre qui avale couramment cinq litres d’eau, puis cinq petites grenouilles, devant les yeux des badauds ébahis, avant de les recracher, flotte et rainettes bien vivantes, et de faire sa quête…</p>
-          <div class="timeline-item-meta"><span class="timeline-item-author">Félix Bayart</span>, <span class="timeline-item-comments-count">18 comments</span></div>
-        </div>
-      </a>
-    </div>
-    <div class="timeline-item">
-      <div class="timeline-author-date"><span>16/02/2013</span></div>
-      <a href="blog.html" class="timeline-author-link">
-        <img alt="avatar" src="images/avatars/avatar.png">
-      </a>
-      <a href="blog.html" class="timeline-item-link">
-        <div class="timeline-item-content">
-          <div class="timeline-item-cover">
-            <img src="images/cover/paris.jpg" alt="paris">
-          </div>
-          <h2>Plongée dans le fantastique social</h2>
-          <p>Connaissez-vous l’homme-aquarium ? C’est un camelot du marché d’Aligre qui avale couramment cinq litres d’eau, puis cinq petites grenouilles, devant les yeux des badauds ébahis, avant de les recracher, flotte et rainettes bien vivantes, et de faire sa quête…</p>
-          <div class="timeline-item-meta"><span class="timeline-item-author">Félix Bayart</span>, <span class="timeline-item-comments-count">18 comments</span></div>
-        </div>
-      </a>
-    </div>
-    <div class="timeline-item">
-      <div class="timeline-author-date"><span>16/02/2013</span></div>
-      <a href="blog.html" class="timeline-author-link">
-        <img alt="avatar" src="images/avatars/avatar.png">
-      </a>
-      <a href="blog.html" class="timeline-item-link">
-        <div class="timeline-item-content">
-          <div class="timeline-item-cover">
-            <img src="images/cover/paris.jpg" alt="paris">
-          </div>
-          <h2>Plongée dans le fantastique social</h2>
-          <p>Connaissez-vous l’homme-aquarium ? C’est un camelot du marché d’Aligre qui avale couramment cinq litres d’eau, puis cinq petites grenouilles, devant les yeux des badauds ébahis, avant de les recracher, flotte et rainettes bien vivantes, et de faire sa quête…</p>
-          <div class="timeline-item-meta"><span class="timeline-item-author">Félix Bayart</span>, <span class="timeline-item-comments-count">18 comments</span></div>
-        </div>
-      </a>
-    </div>
+        </a>
+      </div>
+    <?php } ?>
   </div>
 
   <?php $this->load->view($template . 'element/content/sidebar'); ?>
