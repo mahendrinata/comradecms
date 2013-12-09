d945abc5ede8834a719c8ff1b1dde95953c6600f
Mahendri Winata <mahen.0112@gmail.com>
2013-12-09 13:35:29 +0700

    fix get content by category

--- comradecms/controllers/category.php
+++ comradecms/controllers/category.php
@@ -6,7 +6,10 @@ if (!defined('BASEPATH'))
 class Category extends Public_Controller {
 
   public function index($slug = NULL) {
-    $this->data['contents'] = $this->Content_model->get_contents_by_type($slug);
+    $this->data['contents'] = $this->Content_model->get_contents_by_type($slug, self::$limit, $this->get_offset_from_segment(self::$offset));
+    
+    $this->pagination_create($this->Content_model->count_contents_by_type());
+    
     $this->load->view(self::$layout_default, $this->data);
   }
 
