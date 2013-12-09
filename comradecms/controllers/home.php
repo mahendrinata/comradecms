8fb36b443c50ad582f60f90b9e18c73271637472
Mahendri Winata <mahen.0112@gmail.com>
2013-12-09 12:38:08 +0700

    change pagination create

--- comradecms/controllers/home.php
+++ comradecms/controllers/home.php
@@ -14,8 +14,7 @@ class Home extends Public_Controller {
     $this->load->model('Content_model');    
     $this->data['contents'] = $this->Content_model->get_contents(self::$limit, $this->get_offset_from_segment(self::$offset));
     
-    $count = $this->Content_model->count_contents();
-    $this->pagination_create($count);
+    $this->pagination_create($this->Content_model->count_contents());
     
     $this->data['portfolios'] = $this->Content_model->get_contents_by_type('portfolio', 8);
     
