3c849f776e8892e2c6b5a0a29c919660182f4904
Mahendri Winata <mahen.0112@gmail.com>
2013-12-10 05:28:21 +0700

    change load model

--- comradecms/controllers/Album.php
+++ comradecms/controllers/Album.php
@@ -4,15 +4,18 @@ if (!defined('BASEPATH'))
   exit('No direct script access allowed');
 
 class Album extends Public_Controller {
+  
+  public function __construct() {
+    parent::__construct();
+    $this->load->model('Media_model');
+  }
 
   public function index() {
-    $this->load->model('Media_model');
     $$this->data['albums'] = $this->Media_model->get_albums();
     $this->load->view(self::$layout . 'default', $this->data);
   }
 
   public function gallery($slug = NULL) {
-    $this->load->model('Media_model');
     $$this->data['albums'] = $this->Media_model->get_galleries($slug);
     $this->load->view(self::$layout . 'default', $this->data);
   }
